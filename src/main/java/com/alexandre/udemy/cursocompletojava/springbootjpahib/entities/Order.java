434ee5402b20b1cb054b46c6228e8aab6632c53d
Alexandre Pinheiro <alexclemente1985@gmail.com>
2023-06-28 22:53:38 -0300

    OrderStatus Enum

--- src/main/java/com/alexandre/udemy/cursocompletojava/springbootjpahib/entities/Order.java
+++ src/main/java/com/alexandre/udemy/cursocompletojava/springbootjpahib/entities/Order.java
@@ -1,5 +1,6 @@
 package com.alexandre.udemy.cursocompletojava.springbootjpahib.entities;
 
+import com.alexandre.udemy.cursocompletojava.springbootjpahib.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -20,6 +21,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -28,11 +31,22 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public Long getId() {
