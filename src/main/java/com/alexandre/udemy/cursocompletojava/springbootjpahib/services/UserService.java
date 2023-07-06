a8c315f606e74edc92c1dd8438fabeafdda02a11
Alexandre Pinheiro <alexclemente1985@gmail.com>
2023-07-05 21:44:04 -0300

    Exception handler delete

--- src/main/java/com/alexandre/udemy/cursocompletojava/springbootjpahib/services/UserService.java
+++ src/main/java/com/alexandre/udemy/cursocompletojava/springbootjpahib/services/UserService.java
@@ -2,8 +2,11 @@ package com.alexandre.udemy.cursocompletojava.springbootjpahib.services;
 
 import com.alexandre.udemy.cursocompletojava.springbootjpahib.entities.User;
 import com.alexandre.udemy.cursocompletojava.springbootjpahib.repositories.UserRepository;
+import com.alexandre.udemy.cursocompletojava.springbootjpahib.services.exceptions.DatabaseException;
 import com.alexandre.udemy.cursocompletojava.springbootjpahib.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
@@ -31,7 +34,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        } catch(EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch(DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
